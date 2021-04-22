db5d9a56c4ccc89e87f9db830185745a282e59f7
mynamesunpower <mynamesunpower@gmail.com>
2021-04-22 17:55:03 +0900

    backtick 예제

--- 2/src/index.ts
+++ 2/src/index.ts
@@ -8,3 +8,7 @@ let persons: IPerson[] = R.range(0, 2)
     .map((n: number) => new Person(chance.name(), chance.age()))
 
 console.log(persons)
+
+const name: string = '태양'
+const age: number = 32
+console.log(`제 이름은 ${name}이고 ${age}세 입니다.`)
